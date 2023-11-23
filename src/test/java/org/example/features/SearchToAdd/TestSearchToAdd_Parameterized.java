a4d0aa199b4fff73dcf78588ecabc9d08e8c708a
MarcuTamas <tamasmarcu@outlook.com>
2023-11-23 22:06:25 +0200

    Done feature/Test-SearchToAdd

--- src/test/java/org/example/features/SearchToAdd/TestSearchToAdd_Parameterized.java
+++ src/test/java/org/example/features/SearchToAdd/TestSearchToAdd_Parameterized.java
@@ -1,20 +1,21 @@
 package org.example.features.SearchToAdd;
 
 import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
-import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Issue;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.junit.annotations.UseTestDataFrom;
 import org.example.steps.serenity.EndUserSearchToAddSteps;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
-//@RunWith(SerenityParameterizedRunner.class)
-@RunWith(SerenityRunner.class)
+@RunWith(SerenityParameterizedRunner.class)
+@UseTestDataFrom("src/test/resources/features/SearchToAdd/SearchToAdd.csv")
 public class TestSearchToAdd_Parameterized {
     //TODO: REMEBER THAT in the CSV we do not need this time to add quotes around the data (we have tried it was included in the data).
-    //TODO: add strings
+
+    private String queryWord, plantName, expectedLogMessage;
 
     @Managed(uniqueSession = true, driver = "chrome")
     public WebDriver webdriver;
@@ -26,28 +27,23 @@ public class TestSearchToAdd_Parameterized {
     @Issue("#Parameterized_SearchToAddSteps")
     @Test
     public void parameterized_search_to_add_plant() {
-        this.endUserSearchToAddSteps.goto_home_page();
-        this.endUserSearchToAddSteps.click_myPlantsButton();
-        this.endUserSearchToAddSteps.click_addPlantNavBarButton();
-        this.endUserSearchToAddSteps.click_searchToAddButton();
-        this.endUserSearchToAddSteps.enter_queryWord("Olive");
-//        this.endUserSearchToAddSteps.enter_queryWord("asdasdasd");
-        this.endUserSearchToAddSteps.click_searchToAddQueryButton();
-//        System.out.println("Here");
-//        System.out.println(this.endUserSearchToAddSteps.check_selectFoundedPlantsListButtonIsPresent());
+        this.endUserSearchToAddSteps.goto_home_page_and_then_to_add_plant_page();
 
+        this.endUserSearchToAddSteps.click_searchToAddButton();
+        this.endUserSearchToAddSteps.enter_queryWord(queryWord);
+        this.endUserSearchToAddSteps.click_searchToAddQueryButton();
 
         //Check the case when no plants are found
         if (!this.endUserSearchToAddSteps.check_selectFoundedPlantsListButtonIsPresent()) {
             this.endUserSearchToAddSteps.close_noFoundedPlantsModal();
-            this.endUserSearchToAddSteps.check_logContains("PlantService: fetched 0 plants in rest API.");
+            this.endUserSearchToAddSteps.check_logContains(expectedLogMessage);
         } else {
             this.endUserSearchToAddSteps.click_selectFoundedPlantsList();
-            this.endUserSearchToAddSteps.click_foundedPlantButton("Arbequina European Olive");
-            this.endUserSearchToAddSteps.click_addPlantButton("Arbequina European Olive");
+            this.endUserSearchToAddSteps.click_foundedPlantButton(plantName);
+            this.endUserSearchToAddSteps.click_addPlantButton(plantName);
 
-            //TODO: aici la log messege este ok, doar că o să pun în csv altul diferit în funcție de test.
-            this.endUserSearchToAddSteps.check_the_lastLogMessage("PlantAddComponent: error adding Arbequina European Olive");
+            //We check for the expected log message
+            this.endUserSearchToAddSteps.check_the_lastLogMessage(expectedLogMessage);
         }
     }
 }
